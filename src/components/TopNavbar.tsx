e4f5b56eb41ba7e5f515838bead16095c6b0488f
david <davidkimgba@gmail.com>
2023-08-18 18:49:13 -0700

    Use variables in root for colors

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -1,20 +1,25 @@
 import React from "react";
-import "./TopNavbar.css"
-import { NavLink  } from "react-router-dom";
+import "./TopNavbar.css";
+import { NavLink } from "react-router-dom";
 
 export default function Top_Navbar(): React.ReactElement {
   return (
     <nav className="navbar">
-      <a className="site-title" href="/">David's website</a>
+      <a className="site-title" href="/">
+        David's website
+      </a>
       <ul>
+        <li>
+          <NavLink to="/projects"> Projects</NavLink>
+        </li>
         <li>
           <NavLink to="/contacts"> Contacts </NavLink>
         </li>
         <li>
-        <NavLink to="/aboutme"> About Me </NavLink>
+          <NavLink to="/aboutme"> About Me </NavLink>
         </li>
         <li>
-        <NavLink to="/fun"> Fun </NavLink>
+          <NavLink to="/fun"> Fun </NavLink>
         </li>
         <li>
           <NavLink to="/login"> Login</NavLink>
