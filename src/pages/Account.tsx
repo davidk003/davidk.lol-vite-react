683e243c82c75fd9a0965afa1f423be4950124a7
david <davidkimgba@gmail.com>
2023-09-23 16:15:25 -0700

    Starting Accounts page

---
+++ src/pages/Account.tsx
@@ -0,0 +1,13 @@
+import "./Account.css"
+import { supabase } from "../supabaseClient";
+import React from "react";
+
+export default function Account(): React.ReactElement
+{
+    return(
+        <div className="account-container">
+            <h2>Name :</h2>
+            
+        </div>
+    );
+}
