c33ed2a4433c4707f27b59a0c2347b778ab09fc7
david <davidkimgba@gmail.com>
2023-10-01 23:01:30 -0700

    Updating values now works

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AccountCard.css";
 
 export default function AccountCard(prop: {
-  dataMap: Map<string, string>, isUpdating: boolean, setIsUpdating:React.Dispatch<React.SetStateAction<boolean>>
+  dataMap: Map<string, string>;
+  updateAccountCallback: (inputAccount: Map<string,string>) => Promise<void>;
+  isUpdating: boolean;
+  setIsUpdating: React.Dispatch<React.SetStateAction<boolean>>;
 }): React.ReactElement {
+  const [updateInfo, setUpdateInfo] = useState<Map<string,string>>(
+    new Map( [["username", ""], ["full_name", ""], ["website", ""], ["avatar_url", ""]]));
+
   return (
     <div className="accountcard-container">
-      <MutableFieldElement mutable={prop.isUpdating}
+
+      <MutableFieldElement
+        mutable={prop.isUpdating}
         field="username"
         value={prop.dataMap.get("username")}
+        info={updateInfo}
+        setInfo={setUpdateInfo}
       />
-      <MutableFieldElement mutable={prop.isUpdating}
+      <MutableFieldElement
+        mutable={prop.isUpdating}
         field="full_name"
         value={prop.dataMap.get("full_name")}
+        info={updateInfo}
+        setInfo={setUpdateInfo}
       />
-      <MutableFieldElement mutable={prop.isUpdating}
+      <MutableFieldElement
+        mutable={prop.isUpdating}
         field="website"
         value={prop.dataMap.get("website")}
-      /> 
-      <MutableFieldElement mutable={prop.isUpdating}
+        info={updateInfo}
+        setInfo={setUpdateInfo}
+      />
+      <MutableFieldElement
+        mutable={prop.isUpdating}
         field="avatar_url"
         value={prop.dataMap.get("avatar_url")}
+        info={updateInfo}
+        setInfo={setUpdateInfo}
       />
       <p className="accountcard-immutable-field">
         {"id: " + (prop.dataMap.get("id") ? prop.dataMap.get("id") : "empty")}
@@ -32,8 +51,26 @@ export default function AccountCard(prop: {
             : "empty")}
       </p>
       <span>
-      <button onClick={()=>{prop.setIsUpdating(!prop.isUpdating)}}>{prop.isUpdating? "Cancel changes" : "Click to update account info" }</button>
-      {prop.isUpdating ? <button onClick={()=>{console.log("Insert function here")}}> Save Changes</button> : <></>}
+        <button
+          onClick={() => {
+            prop.setIsUpdating(!prop.isUpdating);
+          }}
+        >
+          {prop.isUpdating ? "Cancel changes" : "Click to update account info"}
+        </button>
+        {prop.isUpdating ? (
+          <button
+            onClick={() => {
+              console.log(updateInfo);
+              prop.updateAccountCallback(updateInfo);
+            }}
+          >
+            {" "}
+            Save Changes
+          </button>
+        ) : (
+          <></>
+        )}
       </span>
     </div>
   );
@@ -42,17 +79,37 @@ export default function AccountCard(prop: {
 function MutableFieldElement(prop: {
   field: string;
   value: string | undefined;
-  mutable: boolean
+  mutable: boolean;
+  info: Map<string, string>;
+  setInfo: React.Dispatch<React.SetStateAction<Map<string, string>>>;
 }): React.ReactElement {
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    prop.info.set(prop.field, e.target.value);
+    console.log(prop.info)
+  }
+
   return (
     <div className="accountcard-mutable-field">
-        {prop.mutable? (<div className="input-field">
-        <label htmlFor={prop.field + " field"}>
-            new {prop.field}: 
-        </label>
-        <input id={prop.field + " field"}></input>
-      </div>) : <></>}
-      <p>{prop.field + ": " + (prop.value ? prop.value : "empty")}</p>
+      {prop.mutable ? (
+        <div className="input-field">
+          <label htmlFor={prop.field + " field"}>new {prop.field}:</label>
+          <input
+            type="text"
+            id={prop.field + " field"}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          ></input>
+        </div>
+      ) : (
+        <></>
+      )}
+      {prop.value ? (
+        <p>{prop.field + ": " + prop.value}</p>
+      ) : (
+        <p className="empty-value">{prop.field + ": "}</p>
+      )}
     </div>
   );
 }
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -5,44 +5,63 @@ import { SessionContext } from "../App";
 import { Session } from "@supabase/supabase-js";
 import AccountCard from "../components/AccountCard";
 
-
 export default function Account(): React.ReactElement {
   const currentSession: Session | null = useContext<Session | null>(SessionContext);
   const [accountMap, setAccountMap] = useState<Map<string, string> | null>(null);
   const [isUserUpdating,setIsUserUpdating] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchAccount = async () => {
-      let { data: profiles, error } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", currentSession?.user.id)
-        .single();
+  const fetchAccount = async () => {
+    let { data: profiles, error } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", currentSession?.user.id)
+      .single();
 
-      console.log(error ? error : profiles);
-      if (profiles) {
-        let tempMap = new Map<string, string>();
-        Object.keys(profiles).forEach((key) => tempMap.set(key, profiles[key]));
-        setAccountMap(new Map(tempMap));
-      }
-    };
+    console.log(error ? error.message : profiles);
+    if (profiles) {
+      let tempMap = new Map<string, string>();
+      Object.keys(profiles).forEach((key) => tempMap.set(key, profiles[key]));
+      setAccountMap(new Map(tempMap));
+    }
+  };
+  useEffect(() => {
     fetchAccount();
   }, []);
+
+  const updateAccount = async (inputAccount:Map<string,string>) => 
+  {
+    for(let [key, value] of inputAccount)
+    {
+      if (value === "")
+      {
+        inputAccount.delete(key);
+      }
+    }
+    const { error } = await supabase
+      .from("profiles")
+      .update(Object.fromEntries(inputAccount))
+      .eq('id', currentSession?.user.id);
+    if(error)
+    {
+      alert(`Error message: ${error.message}`);
+    }
+    else
+    {
+      setIsUserUpdating(false);
+      fetchAccount();
+    }
+    console.log(error ? error.message : "Account updated");
+  }
 
   return (
     <div className="account-container">
       <h2>Account info:</h2>
       {accountMap ? 
-        (<AccountCard dataMap={accountMap} setIsUpdating={setIsUserUpdating} isUpdating={isUserUpdating}/>)
+        (<AccountCard dataMap={accountMap} updateAccountCallback={updateAccount} setIsUpdating={setIsUserUpdating} isUpdating={isUserUpdating}/>)
       : (
         <p>No account logged in.</p>
       )}
     </div>
   );
 
-  // (
-  //   Array.from(accountMap.keys()).map((key) => (
-  //     <p>{key + ": " + accountMap.get(key)}</p>
-  //   ))
-  // )
 }
